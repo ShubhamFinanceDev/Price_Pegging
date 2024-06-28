101a9f556632a69901309e54f98f8b6830918f5d
saurabh17kumar <saurabhsingh2757@gmail.com>
2024-06-28 16:43:11 +0530

    changing colour pattern

--- src/main/java/com/price/pegging/Utilitty/DsaUtility.java
+++ src/main/java/com/price/pegging/Utilitty/DsaUtility.java
@@ -68,7 +68,7 @@ public class DsaUtility {
                 "        CASE \n" +
                 "            WHEN b.rate_per_sqft BETWEEN a.minimum_rate AND a.maximum_rate THEN 'G' \n" +
                 "            WHEN b.rate_per_sqft BETWEEN (a.minimum_rate - (a.minimum_rate * 15) / 100) AND (a.maximum_rate - (a.maximum_rate * 15) / 100) THEN 'Y'\n" +
-                "            ELSE 'Y' \n" +
+                "            ELSE 'R' \n" +
                 "        END AS flag \n" +
                 "    FROM \n" +
                 "        price_pegging a \n" +
